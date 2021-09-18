3802eb6c1c18aebbd2cb23ed4a391f22f218b679
Gustavo Eidt <gustavo.eidt@gmail.com>
2021-09-18 17:47:15 -0300

    basic graph

--- senac/src/components/IpcaGraphic/IpcaGraphic.js
+++ senac/src/components/IpcaGraphic/IpcaGraphic.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Bar } from "react-chartjs-2";
+import { ipcaData } from "./../../data/ipcaData";
 import "./styles.css";
 
 const IpcaGraphic = () => {
-  return <div>asd</div>;
+  const data = {
+    labels: ipcaData.map((item) => item.data),
+    datasets: [
+      {
+        label: "IPCA",
+        data: ipcaData.map((item) => item.valor),
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return <Bar data={data} options={options} />;
 };
 export default IpcaGraphic;
--- senac/src/pages/Ipca/Ipca.js
+++ senac/src/pages/Ipca/Ipca.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
 import IpcaSelect from "../../components/IpcaSelect";
+import IpcaGraphic from "../../components/IpcaGraphic/IpcaGraphic";
 import "./styles";
 
 const Ipca = () => {
@@ -10,6 +11,7 @@ const Ipca = () => {
   return (
     <Paper>
       <IpcaSelect />
+      <IpcaGraphic />
     </Paper>
   );
 };
